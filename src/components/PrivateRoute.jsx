57778ccbf6fc7434498009661181563d21bfc773
zia-hussain <syedziashahgillani76@gmail.com>
2024-10-20 17:56:30 +0500

    Auth done !

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,16 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ element: Component }) => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+const PrivateRoute = ({ element: Component, requiredRole }) => {
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
 
-  return isAuthenticated ? Component : <Navigate to="/login" />;
+  // Check if the user is authenticated and has the required role
+  if (isAuthenticated && (!requiredRole || user.role === requiredRole)) {
+    return Component;
+  }
+
+  // If not authenticated or doesn't have the required role, redirect to login
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoute;

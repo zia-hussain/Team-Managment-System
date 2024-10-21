51986ece50f3bb1a03ae54fd9b0a7c3e2a6ca36b
zia-hussain <syedziashahgillani76@gmail.com>
2024-10-22 04:04:40 +0500

    Done

--- src/components/adminDashboard/ManageUsers.jsx
+++ src/components/adminDashboard/ManageUsers.jsx
@@ -10,9 +10,8 @@ import DetailModal from "../Modals/DetailModal";
 import { get, getDatabase, ref, remove, set } from "firebase/database";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { deleteUser } from "firebase/auth";
-import { db } from "../../firebase/firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import { db } from "../../firebase/firebaseConfig";
 
 const ManageUsers = () => {
   const navigate = useNavigate();
@@ -20,12 +19,10 @@ const ManageUsers = () => {
   const { teams, loading, members } = useSelector((state) => state.teams);
   const [memberNames, setMemberNames] = useState({});
   const [selectedTeam, setSelectedTeam] = useState(null);
-  console.log("selectedTeam", selectedTeam?.name);
   const [open, setOpen] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [itemToDelete, setItemToDelete] = useState("");
 
-  // Fetch a single member's name by ID
   const fetchUserName = async (userId) => {
     const db = getDatabase();
     const userRef = ref(db, `users/${userId}`);
@@ -43,10 +40,18 @@ const ManageUsers = () => {
       return "Error";
     }
   };
+
   useEffect(() => {
-    dispatch(fetchTeams(dispatch));
-  }, []);
-  // Fetch all member names for each team and store in state
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchTeams(dispatch));
+      } catch (error) {
+        console.error("Error fetching teams: ", error);
+      }
+    };
+    fetchData();
+  }, [dispatch]);
+
   useEffect(() => {
     const fetchMemberNames = async () => {
       const newMemberNames = {};
@@ -69,20 +74,17 @@ const ManageUsers = () => {
     }
   }, [teams]);
 
-  // Handle modal open
   const handleOpen = (team) => {
     setSelectedTeam(team);
     dispatch(fetchTeamMembers(team.id)); // Fetch members for the selected team
     setOpen(true);
   };
 
-  // Handle modal close
   const handleClose = () => {
     setOpen(false);
     setSelectedTeam(null);
   };
 
-  // Handle delete user or team
   const handleDeleteUser = async (
     teamId,
     userId,
@@ -95,7 +97,6 @@ const ManageUsers = () => {
     }
 
     try {
-      // Get the current team data to find the user to delete
       const teamRef = ref(db, `teams/${teamId}`);
       const teamSnapshot = await get(teamRef);
       handleClose();
@@ -104,25 +105,21 @@ const ManageUsers = () => {
         const teamData = teamSnapshot.val();
         const members = teamData.members || [];
 
-        // Find the index of the member with the matching userId
         const memberIndex = members.findIndex((member) => member.id === userId);
 
         if (memberIndex !== -1) {
-          // Remove the member from the array
           members.splice(memberIndex, 1);
 
-          // Update the team data with the new members list
           await set(teamRef, {
             ...teamData,
-            members, // Save updated members list
+            members,
           });
 
-          // Update the local state to reflect the deletion
           setCurrentMembers((prevMembers) =>
             prevMembers.filter((member) => member.id !== userId)
           );
 
-          dispatch(deleteMember(userId)); // Dispatch the action after successful deletion
+          dispatch(deleteMember(userId));
         } else {
           console.error("User not found in team members", userId);
         }
@@ -150,6 +147,7 @@ const ManageUsers = () => {
     setShowDeleteModal(false);
     handleClose();
   };
+
   const handleBackClick = () => {
     navigate(-1);
   };
@@ -163,77 +161,66 @@ const ManageUsers = () => {
         <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
         Back
       </button>
-      <h1
-        className="text-5xl font-extrabold mb-4 bg-gradient-to-r from-sky-300 to-blue-400
-
- bg-clip-text text-transparent"
-      >
+      <h1 className="text-5xl font-extrabold mb-4 bg-gradient-to-r from-sky-300 to-blue-400 bg-clip-text text-transparent">
         Manage Teams & Users
       </h1>
 
-      {/* Teams Card Grid */}
-      <>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-6xl">
-          {loading ? (
-            <p className="text-white">Loading...</p>
-          ) : (
-            teams?.map((team) => (
-              <div
-                key={team.id}
-                className="bg-gray-800 rounded-lg p-6 shadow-lg hover:bg-gray-700 transition cursor-pointer"
-                onClick={() => handleOpen(team)}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-6xl">
+        {loading ? (
+          <p className="text-white">Loading...</p>
+        ) : teams.length > 0 ? (
+          teams.map((team) => (
+            <div
+              key={team.id}
+              className="bg-gray-800 rounded-lg p-6 shadow-lg hover:bg-gray-700 transition cursor-pointer"
+              onClick={() => handleOpen(team)}
+            >
+              <div className="flex justify-between items-center">
+                <h2 className="text-2xl font-bold">{team.name}</h2>
+              </div>
+              <p className="text-gray-400 mt-2">
+                Created on: {team.members ? team.members.length : 0}
+              </p>
+              <button
+                className="mt-4 bg-red-500 text-white p-2 rounded-lg hover:bg-red-600 transition-colors w-full"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDeleteTeam(team.id);
+                }}
               >
-                <div className="flex justify-between items-center">
-                  <h2 className="text-2xl font-bold">{team.name}</h2>
-                </div>
-                <h4 className="text-white">
-                  Members: {team.members ? team.members.length : 0}
-                </h4>
-                <p className="text-gray-400 mt-2">
-                  Created on: {new Date(team.createdAt).toLocaleDateString()}
-                </p>
-                <button
-                  className="mt-4 bg-red-500 text-white p-2 rounded-lg hover:bg-red-600 transition-colors w-full"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleDeleteTeam(team.id);
-                  }}
-                >
-                  <FontAwesomeIcon icon={faTrash} /> Delete Team
-                </button>
-              </div>
-            ))
-          )}
-        </div>
+                <FontAwesomeIcon icon={faTrash} /> Delete Team
+              </button>
+            </div>
+          ))
+        ) : (
+          <p className="text-white">No teams found.</p>
+        )}
+      </div>
 
-        {/* Delete confirmation modal */}
-        {/* Delete confirmation modal */}
-        {showDeleteModal && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-gray-700 rounded-lg p-6 w-1/3">
-              <h2 className="text-lg font-semibold mb-4">Confirm Deletion</h2>
-              <p>Are you sure you want to delete?</p>{" "}
-              {/* Update the text to reflect the item */}
-              <div className="flex justify-between mt-4">
-                <button
-                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-                  onClick={() => {
-                    confirmDelete(); // Call confirmDelete function on delete
-                  }}
-                >
-                  Delete
-                </button>
-                <button
-                  className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
-                  onClick={() => setShowDeleteModal(false)} // Pass function reference correctly
-                >
-                  Cancel
-                </button>
-              </div>
+      {/* Delete confirmation modal */}
+      {showDeleteModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-gray-700 rounded-lg p-6 w-1/3">
+            <h2 className="text-lg font-semibold mb-4">Confirm Deletion</h2>
+            <p>Are you sure you want to delete?</p>
+            <div className="flex justify-between mt-4">
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                onClick={confirmDelete}
+              >
+                Delete
+              </button>
+              <button
+                className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
+                onClick={() => setShowDeleteModal(false)}
+              >
+                Cancel
+              </button>
             </div>
           </div>
-        )}
-      </>
+        </div>
+      )}
+
       {open && (
         <DetailModal
           selectedTeam={selectedTeam}

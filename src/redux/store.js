d54a91ec7626a01034e2e10ff8b57b60b4e13071
zia-hussain <syedziashahgillani76@gmail.com>
2024-10-21 16:50:28 +0500

    working ...

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice"; // Import the auth slice reducer
 import { thunk } from "redux-thunk"; // Import thunk
+import userReducer from "./features/userSlice";
+import teamsReducer from "./teamsReducer";
 
 const store = configureStore({
   reducer: {
     auth: authReducer, // Add auth reducer
+    users: userReducer,
+    teams: teamsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // Add thunk middleware
 });
